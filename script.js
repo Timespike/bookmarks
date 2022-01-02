4f6c4ca0cef79e14c5a81b57c1fe73607291e69c
Timespike <50344633+Timespike@users.noreply.github.com>
2022-01-02 14:31:48 -0600

    adding constants to JS

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('testing');
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+
